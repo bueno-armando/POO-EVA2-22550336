30918027ce2c90c07c57df9552075a9cfc989496
bueno-armando <124701314+bueno-armando@users.noreply.github.com>
2023-03-08 12:49:13 -0600

    Add files via upload

---
+++ EVA2_4_FORMATO_APA/src/eva2_4_formato_apa/Articulo.java
@@ -0,0 +1,28 @@
+package eva2_4_formato_apa;
+
+/**
+ *
+ * @author Armando Bueno
+ */
+public class Articulo extends Documento {
+    String magazine;
+    String artNumber;
+
+    public String getMagazine() {
+        return magazine;
+    }
+
+    public void setMagazine(String magazine) {
+        this.magazine = magazine;
+    }
+
+    public String getArtNumber() {
+        return artNumber;
+    }
+
+    public void setArtNumber(String artNumber) {
+        this.artNumber = artNumber;
+    }
+    
+    
+}
---
+++ EVA2_4_FORMATO_APA/src/eva2_4_formato_apa/SitioWeb.java
@@ -0,0 +1,30 @@
+package eva2_4_formato_apa;
+
+/**
+ *
+ * @author Armando Bueno
+ */
+public class SitioWeb extends Documento{
+    private String url;
+    private String accessDate;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getAccessDate() {
+        return accessDate;
+    }
+
+    public void setAccessDate(String accessDate) {
+        this.accessDate = accessDate;
+    }
+
+    
+    
+    
+}
