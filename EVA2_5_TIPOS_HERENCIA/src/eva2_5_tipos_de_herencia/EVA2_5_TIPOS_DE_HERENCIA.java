94c73ff41d8c2c800fe9ea36857b872c23f2fe4d
bueno-armando <124701314+bueno-armando@users.noreply.github.com>
2023-03-10 09:21:43 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_HERENCIA/src/eva2_5_tipos_de_herencia/EVA2_5_TIPOS_DE_HERENCIA.java
@@ -0,0 +1,25 @@
+package eva2_5_tipos_de_herencia;
+
+/**
+ *
+ * @author Armando Bueno
+ */
+public class EVA2_5_TIPOS_DE_HERENCIA {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    class SuperClase{
+    
+    }
+
+    class OtraSuperClase{//NO SE PUEDE HEREDAR DE DOS O MÁS SUPERCLASES
+
+    }
+                    //extends solo funciona para una unica clase
+    class BaseClase extends SuperClase{
+
+    }
+}
