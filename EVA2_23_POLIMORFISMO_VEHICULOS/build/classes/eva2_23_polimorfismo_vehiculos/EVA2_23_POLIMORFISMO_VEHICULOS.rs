7fe8287e703e0ae3c248c2558d3351dc3d847199
bueno-armando <124701314+bueno-armando@users.noreply.github.com>
2023-03-30 09:42:51 -0600

    Subir practicas 23-25 (30 marzo)

---
+++ EVA2_23_POLIMORFISMO_VEHICULOS/build/classes/eva2_23_polimorfismo_vehiculos/EVA2_23_POLIMORFISMO_VEHICULOS.rs
@@ -0,0 +1,5 @@
+eva2_23_polimorfismo_vehiculos.EVA2_23_POLIMORFISMO_VEHICULOS
+eva2_23_polimorfismo_vehiculos.Vehiculo
+eva2_23_polimorfismo_vehiculos.Automovil
+eva2_23_polimorfismo_vehiculos.DatosVehiculo
+eva2_23_polimorfismo_vehiculos.Bicicleta
---
+++ EVA2_24_COLLECTIONS/build/classes/eva2_24_collections/EVA2_24_COLLECTIONS.rs
@@ -0,0 +1,4 @@
+eva2_24_collections.Persona
+eva2_24_collections.Perro
+eva2_24_collections.Automovil
+eva2_24_collections.EVA2_24_COLLECTIONS
